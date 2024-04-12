7ce89d617e2a4630e0b4cca8136ff621f0ef359d
Leo Wirasanto Laia <leowirasanto@gmail.com>
2024-04-12 12:39:59 +0800

    initial commit

---
+++ SimpleAIChat/SimpleAIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAIChatApp.swift
+//  SimpleAIChat
+//
+//  Created by Leo Wirasanto Laia on 12/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
